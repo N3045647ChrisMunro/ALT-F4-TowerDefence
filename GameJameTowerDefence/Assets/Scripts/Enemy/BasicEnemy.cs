859d4ff4b4a6a3cceac59eea3d5acdcd2af197fc
Chris <N3045647@live.tees.ac.uk>
2016-04-18 23:38:57 +0100

    First Commit:  - Unity 5.3  - Base Enemy Class  - Basic Enemy Movement

---
+++ GameJameTowerDefence/Assets/Scripts/Enemy/BasicEnemy.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemy : EnemyBase{
+
+    private int currentWaypoint = 0;
+    Transform targetWaypoint;
+
+	// Use this for initialization
+    void Start()
+    {
+        this.type = "basic";
+        this.health = 10;
+        this.moveSpeed = 2.5f;
+
+        this.wayPoints = new Transform[3];
+
+        //TODO: Clean this up
+        this.wayPoints[0] = GameObject.Find("1").transform;
+        this.wayPoints[1] = GameObject.Find("2").transform;
+        this.wayPoints[2] = GameObject.Find("3").transform;
+
+    }
+	
+	// Update is called once per frame
+
+    void Update()
+    {
+        if (currentWaypoint < this.wayPoints.Length)
+        {
+            if (targetWaypoint == null)
+            {
+                targetWaypoint = this.wayPoints[currentWaypoint];
+            }
+        }
+
+        //check to see if we hit the last waypoint
+        if (currentWaypoint >= this.wayPoints.Length)
+        {
+            currentWaypoint = 0;
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            move();
+        }
+
+
+    }
+
+    public override void move()
+    {
+        //Rotate to target 
+        transform.forward = Vector3.RotateTowards(transform.forward, targetWaypoint.position - transform.position, 4f * Time.deltaTime, 0.0f); 
+
+        //Move towards the next waypoints position
+        transform.position = Vector3.MoveTowards(transform.position, targetWaypoint.position, this.moveSpeed * Time.deltaTime);
+
+        //Check to see if we reached the waypoint
+        if (transform.position == targetWaypoint.position)
+        {
+            currentWaypoint++;
+            targetWaypoint = this.wayPoints[currentWaypoint];
+
+        }
+
+    }
+
+}
---
+++ GameJameTowerDefence/Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBase : MonoBehaviour{
+
+
+    private string type_;
+    private int health_;
+    private float moveSpeed_;
+
+    private Transform[] wayPoints_;
+
+    public string type
+    {
+        get { return type_; }
+        set { type_ = value; }
+    }
+
+    public int health
+    {
+        get { return health_; }
+        set { health_ = value; }
+    }
+
+    public float moveSpeed
+    {
+        get { return moveSpeed_; }
+        set { moveSpeed_ = value; }
+    }
+
+    public Transform[] wayPoints
+    {
+        get { return wayPoints_; }
+        set { wayPoints_ = value; }
+    }
+
+    public EnemyBase() 
+    {
+
+    }
+
+    public virtual void move()
+    {
+        
+    }
+
+}
